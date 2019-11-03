5895604265047bd2e02167df7fb34714f3efa919
Miguel Savignano <miguel.savignano@the-cocktail.com>
2019-11-04 00:16:09 +0100

    merge with octokit

--- index.rb
+++ index.rb
@@ -4,11 +4,11 @@ require 'octokit'
 @event = JSON.parse(File.read(ENV['GITHUB_EVENT_PATH']))
 puts @event
 
-# @repository = ENV['GITHUB_REPOSITORY']
-# @base_brach = ENV['INPUT_BASE_BRACH'] || 'develop'
-# @head_to_merge = ENV['GITHUB_SHA'] # or brach name
-# @github_token = ENV['GITHUB_TOKEN']
+@repository = ENV['GITHUB_REPOSITORY']
+@base_brach = ENV['INPUT_BASE_BRACH'] || 'develop'
+@head_to_merge = ENV['GITHUB_SHA'] # or brach name
+@github_token = ENV['GITHUB_TOKEN']
 
-# @client = Octokit::Client.new(access_token: @github_token)
+@client = Octokit::Client.new(access_token: @github_token)
 
-# @client.merge(@repository, @base_brach, @head_to_merge)
+@client.merge(@repository, @base_brach, @head_to_merge)
