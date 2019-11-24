f1790b578691aac2855ff92bea0d811ef031fe02
miguemasx <migue.masx@gmail.com>
2019-11-24 02:42:01 +0100

    find base branch name

--- index.rb
+++ index.rb
@@ -1,18 +1,40 @@
 require 'json'
 require 'octokit'
 
-@event = JSON.parse(File.read(ENV['GITHUB_EVENT_PATH']))
-puts @event&.to_json
+COMMENT_REGEXP = /\/merge to/
+BRANCH_REGEXP = /\merge to ([0-9a-zA-Z'-\/]+)/
+COMMAND_NAME = "/merge"
 
-@repository = ENV['GITHUB_REPOSITORY']
-@base_brach = ENV['INPUT_BASE_BRACH'] || 'develop'
+@event = JSON.parse(File.read(ENV['GITHUB_EVENT_PATH']))
+
 @head_to_merge = ENV['GITHUB_SHA'] # or brach name
+@repository = ENV['GITHUB_REPOSITORY']
 @github_token = ENV['GITHUB_TOKEN']
+
+def match_branch_name(str)
+  arr = str.split(/\s+/)
+  index = arr.index(COMMAND_NAME)
+  arr[index + 2]
+end
+
+def base_branch
+  comment_message = @event&.dig('comment', 'body')
+  base_branch_name =
+    if comment_message && comment_message =~ COMMENT_REGEXP
+      match_branch_name(comment_message)
+    else
+      ENV['INPUT_BASE_BRACH'] || 'develop'
+    end
+  raise unless base_branch_name
+
+rescue StandardError => e
+  raise "Could not find branch name, #{e.message}"
+end
 
 def merge_to(base_branch_name)
   @client = Octokit::Client.new(access_token: @github_token)
   @client.merge(@repository, base_branch_name, @head_to_merge)
-  puts "Finish merge brach #{@head_to_merge}"
+  puts "Finish merge brach #{base_branch_name}"
 end
 
-merge_to(@base_brach)
+merge_to(base_branch)
