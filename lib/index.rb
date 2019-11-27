b2a56f9524fd4fb3674ad77e4a8e79652e7c1315
Miguel Savignano <migue.masx@gmail.com>
2019-11-27 11:59:43 +0100

    Trigger with any event (#1)
    * change finish message
    
    * now type, not need check label name
    
    * update action yaml with inputs

--- lib/index.rb
+++ lib/index.rb
@@ -8,7 +8,7 @@ require_relative './services/merge_branch_service'
 @github_token = ENV['GITHUB_TOKEN']
 @target_branch = ENV['INPUT_TARGET_BRANCH']
 @label_name = ENV['INPUT_LABEL_NAME']
-@type = ENV['INPUT_TYPE'] || 'labeled' # labeled | comment | push
+@type = ENV['INPUT_TYPE'] || 'labeled' # labeled | comment | now
 
 service = MergeBrachService.new(
   event: @event, type: @type, target_branch: @target_branch, label_name: @label_name
@@ -18,7 +18,7 @@ service_target_branch = service.ensure_target_branch
 if service_target_branch
   @client = Octokit::Client.new(access_token: @github_token)
   @client.merge(@repository, service_target_branch, @head_to_merge)
-  puts "Finish merge brach #{service_target_branch}"
+  puts "Finish merge brach to #{service_target_branch}"
 else
   puts 'Skip'
 end
