dd917b13265c66c6e8396438d19e4870314b2acf
Miguel Savignano <migue.masx@gmail.com>
2020-11-02 06:24:44 +0100

    check input type

--- lib/index.rb
+++ lib/index.rb
@@ -8,12 +8,12 @@ def presence(value)
 end
 
 @event = JSON.parse(File.read(ENV['GITHUB_EVENT_PATH']))
-@head_to_merge = presence(ENV['INPUT_HEAD_TO_MERGE']) || presence(ENV['INPUT_FROM_BRANCH']) || ENV['GITHUB_SHA'] # or brach name
+@head_to_merge = presence(ENV['INPUT_HEAD_TO_MERGE']) || presence(ENV['INPUT_FROM_BRANCH']) || presence(ENV['GITHUB_SHA']) # or brach name
 @repository = ENV['GITHUB_REPOSITORY']
-@github_token = presence(ENV['INPUT_GITHUB_TOKEN']) || ENV['GITHUB_TOKEN']
+@github_token = presence(ENV['INPUT_GITHUB_TOKEN']) || presence(ENV['GITHUB_TOKEN'])
 
 inputs = {
-  type: ENV['INPUT_TYPE'] || MergeBrachService::TYPE_LABELED, # labeled | comment | now
+  type: presence(ENV['INPUT_TYPE']) || MergeBrachService::TYPE_LABELED, # labeled | comment | now
   label_name: ENV['INPUT_LABEL_NAME'],
   target_branch: ENV['INPUT_TARGET_BRANCH']
 }
