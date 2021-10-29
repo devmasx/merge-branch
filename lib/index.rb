f8e6f7fb20cfe0af1ce541d57195c29047e75d5d
Miguel Savignano <migue.masx@gmail.com>
2021-10-29 13:28:34 +0200

    .

--- lib/index.rb
+++ lib/index.rb
@@ -34,5 +34,6 @@ if service.valid?
   @client.merge(@repository, inputs[:target_branch], @head_to_merge, ENV['INPUT_MESSAGE'] ? {commit_message: ENV['INPUT_MESSAGE']} : {})
   puts "Completed: Finish merge branch to #{inputs[:target_branch]}"
 else
+  puts "Neutral skip merge target_branch: #{inputs[:target_branch]} @head_to_merge: #{@head_to_merge}"
   puts "Neutral: skip merge target_branch: #{inputs[:target_branch]} @head_to_merge: #{@head_to_merge}"
 end
