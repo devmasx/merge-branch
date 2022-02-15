6589048407154f1508f3bae8304a8fda66775410
Brad Holland <bholland@bamboohealth.com>
2022-02-15 10:24:21 -0500

    SET env value to pass data between steps

--- lib/index.rb
+++ lib/index.rb
@@ -29,16 +29,33 @@ if service.valid?
   @client = Octokit::Client.new(access_token: @github_token)
 
   page_number = 1
+  merge_success = []
+  merge_failure = []
+
   while (page = @client.branches(@repository, page: page_number)).length.positive?
     page.each do |branch|
       if branch.name.match?(inputs[:target_branch])
         puts "Running perform merge target_branch: #{branch.name} @head_to_merge: #{@head_to_merge}}"
-        @client.merge(@repository, branch.name, @head_to_merge, ENV['INPUT_MESSAGE'] ? {commit_message: ENV['INPUT_MESSAGE']} : {})
-        puts "Completed: Finish merge branch #{@head_to_merge} to #{inputs[:target_branch]}"
+
+        if @client.merge(@repository, branch.name, @head_to_merge, ENV['INPUT_MESSAGE'] ? {commit_message: ENV['INPUT_MESSAGE']} : {})&.key?(sha)
+          puts "Completed: Finish merge branch #{@head_to_merge} to #{branch.name}"
+          merge_success << branch.name
+        else
+          puts "Error: Failed to merge branch #{@head_to_merge} to #{branch.name}"
+          merge_failure << branch.name
+        end
       end
     end
     page_number += 1
   end
+
+  # github uses an env file for passing values as environment variables between concurrent job steps
+  File.open(ENV['GITHUB_ENV'], 'a') do |env|
+    env << "MERGE_SUCCESS=#{merge_success.join(', ')}"
+    env << "MERGE_FAILURE=#{merge_failure.join(', ')}"
+  end
+
+  return merge_failure.length.positive? ? 1 : 0
 else
   puts "Neutral: skip merge target_branch: #{inputs[:target_branch]} @head_to_merge: #{@head_to_merge}"
 end
